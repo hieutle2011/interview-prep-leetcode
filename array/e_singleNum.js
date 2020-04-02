33cb23a548798f61a7e670376e3d2684d6360d6a
hieu <hieuletrung102@gmail.com>
2020-04-02 09:47:54 +0700

    improve searching by hash map

--- array/e_singleNum.js
+++ array/e_singleNum.js
@@ -5,13 +5,33 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function (nums) {
+var sortList = function (nums) {
     const sorted = nums.sort((a, b) => a - b)
     while (sorted[0] === sorted[1]) {
         sorted.shift(sorted[0])
         sorted.shift(sorted[1])
-    } 
+    }
     return sorted[0]
 };
 
-module.exports = singleNumber
+// Improve searching by hash map
+var hashTable = function (nums) {
+    const hash_table = {}
+    for (let i = 0; i < nums.length; i++) {
+        const curKey = nums[i].toString()
+        const hashKeys = Object.keys(hash_table)
+        if (hashKeys.includes(curKey)) hash_table[curKey] += 1
+        else hash_table[nums[i]] = 1
+    }
+
+    const keys = Object.keys(hash_table)
+    for (let i = 0; i < keys.length; i++) {
+        if (hash_table[keys[i]] === 1) return parseInt(keys[i])
+    }
+
+}
+
+module.exports = {
+    sortList,
+    hashTable,
+}
