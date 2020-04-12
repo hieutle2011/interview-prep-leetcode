181fa12f28e5e47f1e14b25e42f91d48c417084b
hieu <hieuletrung102@gmail.com>
2020-04-12 10:33:07 +0700

    edit tree test

--- test/e_tree.js
+++ test/e_tree.js
@@ -3,21 +3,22 @@ const expect = require('chai').expect
 const TreeNode = require('../tree')
 
 describe('Tree (Easy Collection) @wip', () => {
-    // Initialize a tree
-    const root = new TreeNode(3)
-    root.left = new TreeNode(9)
-    root.right = new TreeNode(20)
-    root.right.left = new TreeNode(15)
-    root.right.right = new TreeNode(7)
-
 
     describe('#maxDepth', () => {
+        // Initialize a tree
+        const root = new TreeNode(3)
+        root.left = new TreeNode(9)
+        root.right = new TreeNode(20)
+        root.right.left = new TreeNode(15)
+        root.right.right = new TreeNode(7)
+        //      3
+        //     / \
+        //    9  20
+        //  /  \
+        // 15   7
         const maxDepth = require('../tree/e_maxDepth')
         it('Should be correct array tc1', () => {
-            const exp = 3
-
-            const result = maxDepth(root)
-            expect(result).to.eql(exp)
+            expect(maxDepth(root)).to.eql(3)
         })
     })
 })
