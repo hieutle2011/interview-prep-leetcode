899d4fe2e5c29c4e004006d8c0e1c80f2a5bcdaf
hieu <hieuletrung102@gmail.com>
2020-06-11 21:35:24 +0700

    add isValidSudoku

---
+++ python/module_strings/easy.py
@@ -0,0 +1,98 @@
+from typing import List
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        """Given a string, find the first non-repeating character in it
+        and return it's index. If it doesn't exist, return -1."""
+
+        if s == "":
+            return -1
+
+        import string
+        # assume the string contain only lowercase English letters.
+        full = string.ascii_lowercase
+
+        for i in range(len(s)):
+            if s.rfind(s[i]) == i and s[i] in full:
+                return i
+            # for checked character, replace it with empty string
+            full = full.replace(s[i], '')
+
+        # If reaching this line, found none
+        return -1
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        helper = Helper()
+        length = len(board)
+
+        for i in range(length):
+            # check row
+            row = board[i]
+            check_row = helper.checkValidSudokuList(row)
+            if check_row == False:
+                return check_row
+
+            # check col
+            col = []
+            for j in range(length):
+                col.append(board[j][i])
+            check_col = helper.checkValidSudokuList(col)
+            if check_col == False:
+                return check_col
+
+            # check grid
+            if i % 3 == 0:
+                for j in range(0, length, 3):
+                    grid = []
+                    grid.append(board[i][j])
+                    grid.append(board[i+1][j+1])
+                    grid.append(board[i+2][j+2])
+
+                    grid.append(board[i+1][j])
+                    grid.append(board[i+2][j])
+
+                    grid.append(board[i][j+1])
+                    grid.append(board[i][j+2])
+
+                    grid.append(board[i+2][j+1])
+                    grid.append(board[i+1][j+2])
+
+                    check_grid = helper.checkValidSudokuList(grid)
+                    if check_grid == False:
+                        return check_grid
+
+        # If reaching this line, found valid sudoku board
+        return True
+
+    def isValidSudokuV2(self, board: List[List[str]]) -> bool:
+        """Faster version"""
+        column_set = [set() for i in range(9)]
+        grid_set = [set() for i in range(9)]
+        for i in range(9):
+            row_set = set()
+            for j in range(9):
+                num = board[i][j]
+                if num != ".":
+                    # print(i, j)
+                    if num in row_set:
+                        return False
+                    else:
+                        row_set.add(num)
+                    if num in column_set[j]:
+                        return False
+                    else:
+                        column_set[j].add(num)
+                    if num in grid_set[int(i//3*3+j//3)]:
+                        return False
+                    else:
+                        grid_set[int(i//3*3+j//3)].add(num)
+        return True
+
+
+class Helper:
+    def checkValidSudokuList(self, arr: List) -> bool:
+        filter_arr = list(filter(lambda item: item != ".", arr))
+        if len(filter_arr) != len(set(filter_arr)):
+            return False
+        return True
---
+++ python/tests/test_easy_strings.py
@@ -0,0 +1,62 @@
+import unittest
+
+from python.module_strings.easy import Solution
+solution = Solution()
+
+
+class TestFirstUniqueChar(unittest.TestCase):
+    def test_first_unique(self):
+        s = "leetcode"
+        result = solution.firstUniqChar(s)
+        self.assertEqual(result, 0)
+
+    def test_first_unique_2(self):
+        s = "loveleetcode"
+        result = solution.firstUniqChar(s)
+        self.assertEqual(result, 2)
+
+    def test_first_unique_3(self):
+        s = ""
+        result = solution.firstUniqChar(s)
+        self.assertEqual(result, -1)
+
+    def test_first_unique_4(self):
+        s = "abcabc"
+        result = solution.firstUniqChar(s)
+        self.assertEqual(result, -1)
+
+
+class TestValidSudoku(unittest.TestCase):
+    def test_invalid_board(self):
+        board = [
+            ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+        ]
+        result = solution.isValidSudokuV2(board)
+        self.assertEqual(result, False)
+
+    def test_valid_boardself(self):
+        board = [
+            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+        ]
+        result = solution.isValidSudokuV2(board)
+        self.assertEqual(result, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
